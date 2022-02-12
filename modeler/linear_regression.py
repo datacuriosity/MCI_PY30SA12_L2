5ad5a821e488d031267fa98e6cc5c8e166eba67b
nguyen_truong <nmtruong93@gmail.com>
2022-02-12 20:48:03 +0700

    Add lecture 06

--- modeler/linear_regression.py
+++ modeler/linear_regression.py
@@ -1,12 +1,29 @@
+import warnings
 import pandas as pd
+import pickle
 from sklearn.linear_model import LinearRegression
 from data_loader.housing_price_loader import load_boston_data
-import streamlit as st
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 
+warnings.filterwarnings('ignore')
 df = load_boston_data()
-model = LinearRegression()
-model.fit(df.iloc[:, :-1], df.price)
+X_train, X_test, y_train, y_test = train_test_split(df.iloc[:, :-1], df.iloc[:, -1], test_size=0.15, shuffle=True,
+                                                    random_state=4)
 
-prediction = model.predict(df.iloc[:, :-1])
-df_compared = pd.DataFrame({'actual': df.price, 'prediction': prediction})
-print(df_compared)
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+prediction = model.predict(X_test)
+df_compared = pd.DataFrame({'actual': y_test, 'prediction': prediction})
+print(f"Mean squared error, {mean_squared_error(df_compared.actual, df_compared.prediction)}")
+print(f"Mean absolute error, {mean_absolute_error(df_compared.actual, df_compared.prediction)}")
+print(f"Coefficient of determination, {r2_score(df_compared.actual, df_compared.prediction)}")
+
+# with open('../saved_model/housing_price_linear.pickle', 'wb') as f:
+#     pickle.dump(model, f)
+
+with open('../saved_model/housing_price_linear.pickle', 'rb') as f:
+    model_2 = pickle.load(f)
+
+print('DONE')
