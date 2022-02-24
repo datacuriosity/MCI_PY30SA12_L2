7188632ee7e08f10f05d2fe47b55dbf8ccda7422
nguyen_truong <nmtruong93@gmail.com>
2022-02-24 18:05:22 +0700

    Add lecture 09

--- modeler/logistic_regression.py
+++ modeler/logistic_regression.py
@@ -1,4 +1,5 @@
 import warnings
+import numpy as np
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
 from data_loader.housing_price_loader import load_cancer_data
@@ -11,12 +12,14 @@ df = load_cancer_data()
 X_train, X_test, y_train, y_test = train_test_split(df.iloc[:, :-1], df.iloc[:, -1], test_size=0.15, shuffle=True,
                                                     random_state=4)
 
-model = LogisticRegression()
+model = LogisticRegression(penalty='l1', C=50, solver='saga')
 model.fit(X_train, y_train)
 
-prediction = model.predict(X_test)
+# prediction = model.predict(X_test)  # p >= 0.5 --> 1.
+prediction = model.predict_proba(X_test)[:, 1]
+classes = np.where(prediction >= 0.45, 1, 0)
 
-df_compared = pd.DataFrame({'actual': y_test, 'prediction': prediction})
+df_compared = pd.DataFrame({'actual': y_test, 'prediction': classes})
 
 plot_confusion_matrix(df_compared.actual, df_compared.prediction)
 print(classification_report(df_compared.actual, df_compared.prediction))
